fea31991289a080cd1256eb3a1d7d2c214cf18c9
preslav-kalinov <preslav_00_k@abv.bg>
2022-07-09 18:49:52 +0300

    Created Company class

---
+++ include/Company.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#ifndef COMPANY_H
+#define COMPANY_H
+
+class Company {
+    private:
+        std::string name;
+        std::string type;
+        bool isVatRegistered;
+    public:
+        Company();
+        Company(std::string companyName, std::string companyType, bool isCompanyVatRegistered);
+        void setCompanyName(std::string name);
+        std::string getCompanyName();
+        void setCompanyType(std::string type);
+        std::string getCompanyType();
+        void setIsVatRegistered(bool isVatRegistered);
+        bool getIsVatRegistered();
+        void showCompanyData(const Company &company);
+        friend std::ostream& operator << (std::ostream &stream, const Company &company);
+};
+
+#endif
---
+++ src/Company.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdexcept>
+#include "Company.h"
+
+using namespace std;
+
+Company::Company() {
+
+}
+
+Company::Company(string companyName, string companyType, bool isCompanyVatRegistered) {
+	name = companyName;
+	type = companyType;
+	isVatRegistered = isCompanyVatRegistered;
+}
+
+void Company::setCompanyName(string compName) {
+	name = compName;
+}
+
+string Company::getCompanyName() {
+	return name;
+}
+
+void Company::setCompanyType(string compType) {
+	type = compType;
+}
+
+string Company::getCompanyType() {
+	return type;
+}
+
+void Company::setIsVatRegistered(bool isRegistered) {
+	isVatRegistered = isRegistered;
+}
+
+bool Company::getIsVatRegistered() {
+	return isVatRegistered;
+}
+
+void Company::showCompanyData(const Company &company) {
+	cout << "Information for company: " << company.name << "\n";
+	cout << "Company type: " << company.type << "\n";
+	cout << "VAT registered: " << company.isVatRegistered << "\n\n";
+}
+
+ostream& operator << (std::ostream &stream, const Company &company) {
+    stream << "Company name: " << company.name;
+    return stream;
+}
